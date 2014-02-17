9515bfa2f3a18fab7d14f129f0a23dc05f258796
scottmotte <scott@scottmotte.com>
2014-02-17 15:36:31 -0800

    Use previous pattern

--- routes/events.js
+++ routes/events.js
@@ -1,33 +1,37 @@
-module.exports = [
-    {
-	    method  : 'GET',
-	    path    : '/api/v0/events.json',
-	    config  : {
-			handler : function ( request ) {
-				request.Events.fetch( request, function ( err, res ) {
-					if ( err ) return request.reply( err );
-					request.reply( { 
-						success : true,
-						events : res.items
-					});
-				})
-			}
-		}
-    },
-    {
-	    method  : 'POST',
-	    path    : '/api/v0/events.json',
-	    config  : {
-			handler : function ( request ) {
-
-				request.Events.fetch( request, function ( err, res ) {
-					if ( err ) return request.reply( err );
-					request.reply( { 
-						success : true,
-						event : res
-					});
-				})
-			}
-		}
+var events = {
+  all: {
+    handler : function ( request ) {
+      request.Events.fetch( request, function ( err, res ) {
+        if ( err ) return request.reply( err );
+        request.reply( { 
+          success : true,
+          events : res.items
+        });
+      })
     }
+  },
+  create: {
+    handler : function ( request ) {
+      request.Events.fetch( request, function ( err, res ) {
+        if ( err ) return request.reply( err );
+        request.reply( { 
+          success : true,
+          event : res
+        });
+      })
+    }
+  }
+}
+
+module.exports = [
+  {
+    method: 'GET',
+    path: '/api/v0/events.json',
+    config: events.all
+  },
+  {
+    method: 'POST',
+    path: '/api/v0/events.json',
+    config: events.create
+  }
 ];
